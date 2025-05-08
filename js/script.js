e1bea01faf9fa5eef83f61268dfcdca34d6b8bd1
quypq147 <phanngocquy1472004@gmail.com>
2025-05-08 20:12:54 +0700

    up entire project to github repo

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slider-item');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+
+    const offset = -currentIndex * 100;
+    document.querySelector('.slider-container').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Auto-slide every 5 seconds
+setInterval(() => {
+    nextSlide();
+}, 5000);
